045df898b617ae6acde003cc2b9001ef33040104
FB08 <flball@sparcs.org>
2026-05-28 14:24:32 +0900

    softdelete schema/middleware

---
+++ backend/tft/src/db/prisma.extension.ts
@@ -0,0 +1,33 @@
+export const softDeleteExtension = {
+  query: {
+    user: {
+      findMany({ args, query }) {
+        args.where = {
+          ...args.where,
+          deletedAt: null,
+        };
+
+        return query(args);
+      },
+
+      findFirst({ args, query }) {
+        args.where = {
+          ...args.where,
+          deletedAt: null,
+        };
+
+        return query(args);
+      },
+
+      findUnique({ args, query }) {
+        return query({
+          ...args,
+          where: {
+            ...args.where,
+            deletedAt: null,
+          },
+        });
+      },
+    },
+  },
+};
